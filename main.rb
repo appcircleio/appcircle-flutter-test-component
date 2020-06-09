86f43a4a5c37c3e5ba78ef33681a3e5e3453159d
Hasret Sariyer <hasretsariyer@gmail.com>
2020-06-09 10:44:11 +0300

    Update component.yaml and README.md

--- main.rb
+++ main.rb
@@ -5,8 +5,7 @@ def get_env_variable(key)
 	return (ENV[key] == nil || ENV[key] == "") ? nil : ENV[key]
 end
 
-ac_repo_path = get_env_variable("AC_REPOSITORY_DIR") || abort('Missing repo path.')
-ac_flutter_project_path = get_env_variable("AC_FLUTTER_PROJECT_PATH") || "."
+ac_flutter_project_dir = get_env_variable("AC_FLUTTER_PROJECT_DIR") || abort('Missing Flutter project directory.')
 ac_flutter_test_extra_args = get_env_variable("AC_FLUTTER_TEST_EXTRA_ARGS") || "--machine"
 
 def run_command(command)
@@ -31,13 +30,6 @@ def run_command(command)
     return stdout_str
 end
 
-absolute_flutter_project_path = ""
-if Pathname.new("#{ac_flutter_project_path}").absolute?
-    absolute_flutter_project_path = ac_flutter_project_path
-else
-    absolute_flutter_project_path = File.expand_path(File.join(ac_repo_path, ac_flutter_project_path))
-end
-
-run_command("cd #{absolute_flutter_project_path} && flutter test #{ac_flutter_test_extra_args}")
+run_command("cd #{ac_flutter_project_dir} && flutter test #{ac_flutter_test_extra_args}")
 
 exit 0
